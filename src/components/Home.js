2da289490f21d1803df2e24993c34b0badb15d9c
sarat10 <2010030498@klh.edu.in>
2022-06-12 18:26:06 +0530

    movie browser

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <>
+      <Hero text="search your movie" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+         
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
